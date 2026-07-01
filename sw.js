689fc6062be2dfbd0795869ce7c6ddbc102dc76a
nrsr2z7nt9-collab <nrsr2z7nt9@privaterelay.appleid.com>
2026-07-02 06:51:52 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kujo-offline-v2';
+const CACHE = 'kujo-offline-v3';
 const FILES = ['./','index.html','styles.css','standalone.js','print.html','print.css','print.js','manifest.webmanifest','icon.svg'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
